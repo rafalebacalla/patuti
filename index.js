5edf8d908b3d859953b688720489bf05827ede0f
sjfas <raprap.bacalla@gmail.com>
2021-09-22 14:44:26 +0800

    finish firebase env

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const path = require("path");
-
-// import { initializeApp } from "firebase/app";
-var firebase = require("firebase/app")
+require("dotenv").config();
+var firebase = require("firebase/app");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -14,17 +13,18 @@ app.get("/", (req, res) => {
 });
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3j5wI9vBOJqtTtScgbt_K07Pr3MbQzw4",
-  authDomain: "patuti-alien.firebaseapp.com",
-  projectId: "patuti-alien",
-  storageBucket: "patuti-alien.appspot.com",
-  messagingSenderId: "72893915593",
-  appId: "1:72893915593:web:affe56bd0f174269b14818"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("firebase", firebaseConfig)
+  console.log(`App running at port ${port}`);
 });
